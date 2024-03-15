d8ef973fc2d1214c1a2f55c6a7e885e3d64249e5
arajshah <araj.shah@bison.howard.edu>
2024-03-14 20:45:23 -0400

    Comment Changes

--- LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/question1/SecurityOpsDriver.java
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/question1/SecurityOpsDriver.java
@@ -1,6 +1,16 @@
 package org.howard.edu.lsp.midterm.question1;
 
+/**
+ * The SecurityOpsDriver class is responsible for demonstrating the functionality of the SecurityOps class.
+ * It contains a main method that encrypts a given text using the SecurityOps.encrypt method and prints the encrypted text.
+ */
 public class SecurityOpsDriver {
+
+    /**
+     * The main method demonstrates the encryption functionality of the SecurityOps class.
+     * It takes a text as input, encrypts it using the SecurityOps.encrypt method, and prints the encrypted text.
+     * @param args The command-line arguments (not used in this implementation).
+     */
     public static void main(String[] args) {
         // Input text
         String text = "I love CSCI363";
